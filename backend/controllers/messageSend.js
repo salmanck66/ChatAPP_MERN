b854f775f3ed5133771f14b31d94ee36ca08e716
salmanck66 <salmanulfaris.c.k10@gmail.com>
2024-08-05 20:11:42 +0530

    end day

---
+++ backend/controllers/messageSend.js
@@ -0,0 +1,5 @@
+export const sendMessage = async (req, res) => {
+    console.log("Messaged");
+    res.send("Message received");
+  };
+  
--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,18 @@
-import express from 'express'
-import authRoutes from './routes/auth.js'
-import connecttomdbserver from './db/connect.js'
-import { configDotenv } from 'dotenv'
+import express from 'express';
+import authRoutes from './routes/auth.js';
+import messageRoutes from './routes/message.js';
+import connecttomdbserver from './db/connect.js';
+import { configDotenv } from 'dotenv';
 
-const app = express()
+const app = express();
 
-configDotenv()
-app.use(express.json())
-app.use('/api/auth',authRoutes)
-// app.get('/',(req,res)=>
-// {
-//     res.send("Server Is In Home Page")
-// })
-app.listen(3000,()=>
-{
-    connecttomdbserver()
-    console.log("Server is listening");
-})
+configDotenv();
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes);
+
+app.listen(3000, () => {
+  connecttomdbserver();
+  console.log("Server is listening");
+});
