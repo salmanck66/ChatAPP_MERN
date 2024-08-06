f7b42d5da4c336289051ab07633ca8d585d17871
salmanck66 <salmanulfaris.c.k10@gmail.com>
2024-08-06 20:13:37 +0530

    Fixed Issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,7 @@
 export default function App() {
   return (
-    <>
-<button className="btn">
-  <span className="loading loading-spinner"></span>
-  loading
-</button>
-</>
+    <h1 className="text-4xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,13 +1,11 @@
-
-import daisyui from "daisyui"
 /** @type {import('tailwindcss').Config} */
-
-module.exports = {
+export default {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [daisyui],
+  plugins: [],
 }
