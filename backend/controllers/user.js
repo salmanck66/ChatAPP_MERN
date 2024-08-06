0881a46d230de43b89c6fb4f50168bb9237bf68d
salmanck66 <salmanulfaris.c.k10@gmail.com>
2024-08-06 19:00:13 +0530

    deleted usepass

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,8 +1,13 @@
+import User from "../models/user.js";
+
 export const getUserSideBar = async (req, res) => {
-    try {
-        const availableuser = req.user._id
-    } catch (error) {
-        console.log("Error in user",error.message)
-        res.status(500).json({error})
-    }
+  try {
+    const myuser = req.user._id;
+    const usersexeptme = await User.find({_id:{$ne:myuser}}).select("-password");
+
+    res.status(200).json({usersexeptme})
+  } catch (error) {
+    console.log("Error in user", error.message);
+    res.status(500).json({ error });
+  }
 };
